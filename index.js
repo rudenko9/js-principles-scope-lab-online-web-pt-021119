38e99c0364fb735f8ff11251007426031adf87f9
Nataliya R <n.rudenko@gmail.com>
2019-09-10 01:59:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var customerName = 'bob';
 
 function upperCaseCustomerName() {
  
- return customerName = customerName.toUpperCase()
+ return customerName = customerName.toUpperCase();
   }
   
   function setBestCustomer() {
